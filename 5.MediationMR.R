3dce4f658681f266719680af2f1bf185b2986c14
CS7012 <162420506+CS7012@users.noreply.github.com>
2025-11-04 08:10:31 +0800

    Update 5.MediationMR.R

--- 5.MediationMR.R
+++ 5.MediationMR.R
@@ -1,18 +1,3 @@
-#数据与代码声明
-#如果没有购买SCI狂人团队或者生信狂人团队的正版会员
-#没有经过我们的同意，擅自使用我们整理好的数据与代码发文章
-#如果被我们发现你的文章用了我们的数据与代码，我们将使用一切手段让你的文章撤稿
-
-####关注微信公众号生信狂人团队
-###遇到代码报错等不懂的问题可以添加微信scikuangren进行答疑
-###作者邮箱：sxkrteam@shengxinkuangren.com
-
-
-#####################################
-####关注微信公众号生信狂人团队
-###遇到代码报错等不懂的问题可以添加微信scikuangren进行答疑
-###作者邮箱：sxkrteam@shengxinkuangren.com
-
 #install packages
 #install.packages("devtools")
 #devtools::install_github("MRCIEU/TwoSampleMR")
@@ -35,23 +20,9 @@ mr_result<- mr(harm_rt)
 View(mr_result)
 result_or=generate_odds_ratios(mr_result)
 write.table(result_or[,5:ncol(result_or)],"OR37-1.txt",row.names = F,sep = "\t",quote = F)
-#####################################
-####关注微信公众号生信狂人团队
-###遇到代码报错等不懂的问题可以添加微信scikuangren进行答疑
-###作者邮箱：sxkrteam@shengxinkuangren.com
 
 # step 2 outcome to bc
 #直接看前面的分析
-
-#####################################
-#数据与代码声明
-#如果没有购买SCI狂人团队或者生信狂人团队的正版会员
-#没有经过我们的同意，擅自使用我们整理好的数据与代码发文章
-#如果被我们发现你的文章用了我们的数据与代码，我们将使用一切手段让你的文章撤稿
-
-####关注微信公众号生信狂人团队
-###遇到代码报错等不懂的问题可以添加微信scikuangren进行答疑
-###作者邮箱：sxkrteam@shengxinkuangren.com
 
 #step 3 bc to imc
 harm_rt3=read.table("harmonise27.txt",sep = "\t",header = T)
@@ -59,40 +30,13 @@ mr_result3<- mr(harm_rt3)
 View(mr_result3)
 result_or3=generate_odds_ratios(mr_result3)
 write.table(result_or3[,5:ncol(result_or3)],"OR37-3.txt",row.names = F,sep = "\t",quote = F)
-#####################################
-#数据与代码声明
-#如果没有购买SCI狂人团队或者生信狂人团队的正版会员
-#没有经过我们的同意，擅自使用我们整理好的数据与代码发文章
-#如果被我们发现你的文章用了我们的数据与代码，我们将使用一切手段让你的文章撤稿
-
-####关注微信公众号生信狂人团队
-###遇到代码报错等不懂的问题可以添加微信scikuangren进行答疑
-###作者邮箱：sxkrteam@shengxinkuangren.com
-
 
 #step 4 imc to outcome
-#####################################
-####关注微信公众号生信狂人团队
-###遇到代码报错等不懂的问题可以添加微信scikuangren进行答疑
-###作者邮箱：sxkrteam@shengxinkuangren.com
 harm_rt4=read.table("harmonise37.txt",sep = "\t",header = T)
 mr_result4<- mr(harm_rt4)
 View(mr_result4)
 result_or4=generate_odds_ratios(mr_result4)
 write.table(result_or4[,5:ncol(result_or4)],"OR37-4.txt",row.names = F,sep = "\t",quote = F)
-#####################################
-####关注微信公众号生信狂人团队
-###遇到代码报错等不懂的问题可以添加微信scikuangren进行答疑
-###作者邮箱：sxkrteam@shengxinkuangren.com
-#数据与代码声明
-#如果没有购买SCI狂人团队或者生信狂人团队的正版会员
-#没有经过我们的同意，擅自使用我们整理好的数据与代码发文章
-#如果被我们发现你的文章用了我们的数据与代码，我们将使用一切手段让你的文章撤稿
-
-####关注微信公众号生信狂人团队
-###遇到代码报错等不懂的问题可以添加微信scikuangren进行答疑
-###作者邮箱：sxkrteam@shengxinkuangren.com
-
 #####mediation effect
 beta_all=mr_result[3,"b"]
 
@@ -107,17 +51,6 @@ beta12=beta1*beta2
 beta_dir=beta_all-beta12
 
 se=sqrt(mr_result3[3,"b"]^2*mr_result3[3,"se"]^2+mr_result4[3,"b"]^2*mr_result4[3,"se"]^2)
-#####################################
-#数据与代码声明
-#如果没有购买SCI狂人团队或者生信狂人团队的正版会员
-#没有经过我们的同意，擅自使用我们整理好的数据与代码发文章
-#如果被我们发现你的文章用了我们的数据与代码，我们将使用一切手段让你的文章撤稿
-
-####关注微信公众号生信狂人团队
-###遇到代码报错等不懂的问题可以添加微信scikuangren进行答疑
-###作者邮箱：sxkrteam@shengxinkuangren.com
-
-
 #Z
 Z=beta12/se
 
@@ -136,13 +69,3 @@ uci_p=uci/beta_all
 
 meresult=data.frame(beta_all,beta1,beta2,beta12,beta_dir,Z,P,lci,uci,beta12_p,lci_p,uci_p)
 write.table(meresult,"meresult31.txt",quote = F,sep = "\t",row.names = F)
-#####################################
-#数据与代码声明
-#如果没有购买SCI狂人团队或者生信狂人团队的正版会员
-#没有经过我们的同意，擅自使用我们整理好的数据与代码发文章
-#如果被我们发现你的文章用了我们的数据与代码，我们将使用一切手段让你的文章撤稿
-
-####关注微信公众号生信狂人团队
-###遇到代码报错等不懂的问题可以添加微信scikuangren进行答疑
-###作者邮箱：sxkrteam@shengxinkuangren.com
-
